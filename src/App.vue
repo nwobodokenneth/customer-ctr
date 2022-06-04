5c7579ee8fe09bafbc816c2b012d6c01d6ff8ca0
nwobodokenneth <k.nwobodo@semicolon.africa>
2022-06-04 18:38:08 +0100

    project setup

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,12 @@
 <template>
-  <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view/>
-  </div>
+  <v-app>
+    <div id="app">
+      <v-main>
+        <router-view/>
+      </v-main>
+    </div>
+
+  </v-app>
 </template>
 
 <style lang="scss">
